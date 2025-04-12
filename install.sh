25bf2b056225c4e8115d6f86b57655416f77dcff
Kartav__ <69749101+kartavkun@users.noreply.github.com>
2025-04-12 15:38:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,15 @@ default_install() {
     exit 1
   fi
 
-  echo "Запуск install_easy.sh..."
-  if ! /opt/zapret/install_easy.sh; then
-    echo "Ошибка: не удалось запустить install_easy.sh."
-  fi
-
   if [ -f "/sys/fs/selinux/enforce" ]; then 
 	echo "Обнаружены следы selinux. Применяем правила."
 	./module/fixfilecontext.sh
   fi
-
+  
+  echo "Запуск install_easy.sh..."
+  if ! /opt/zapret/install_easy.sh; then
+    echo "Ошибка: не удалось запустить install_easy.sh."
+  fi
 }
 
 # Функции для установки различных конфигов
