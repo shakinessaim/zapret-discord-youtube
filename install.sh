d210c958967b3186127946e398f0bad36900b29b
kartavkun <nikitarobezhko@yandex.ru>
2025-06-13 16:45:20 +0500

    add cloudflare ipset, new configs, new optional lists

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ clear
 
 # Функция для установки конфига по умолчанию
 default_install() {
-  if [ -f "/sys/fs/selinux/enforce" ]; then 
-	echo "Обнаружены следы selinux. Применяем правила."
-	./module/fixfilecontext.sh
+  if [ -f "/sys/fs/selinux/enforce" ]; then
+    echo "Обнаружены следы selinux. Применяем правила."
+    ./module/fixfilecontext.sh
   fi
-  
+
   echo "Запуск install_easy.sh..."
   if ! /opt/zapret/install_easy.sh; then
     echo "Ошибка: не удалось запустить install_easy.sh."
@@ -19,24 +19,24 @@ default_install() {
     echo "Настройка службы zapret для Void Linux через runit..."
     sudo cp -r /opt/zapret/init.d/runit/zapret/ /etc/sv/
     sudo ln -s /etc/sv/zapret /var/service
-    sudo sv up zapret 
+    sudo sv up zapret
     echo "Служба zapret настроена и запущена для Void Linux."
   fi
 
   # Проверка на AntiX Linux и настройка службы через runit или sysVinit
   if [ -f "/usr/local/bin/antix" ]; then
     if ! command -v sv >/dev/null 2>&1; then
-        echo "Настройка службы zapret для AntiX Linux..."
-        sudo ln -s /opt/zapret/init.d/zapret /etc/init.d/
-        sudo service zapret start
-        sudo update-rd.d zapret defaults
-        echo "Служба zapret настроена и запущена для AntiX Linux."
+      echo "Настройка службы zapret для AntiX Linux..."
+      sudo ln -s /opt/zapret/init.d/zapret /etc/init.d/
+      sudo service zapret start
+      sudo update-rd.d zapret defaults
+      echo "Служба zapret настроена и запущена для AntiX Linux."
     else
-        echo "Настройка службы zapret для AntiX Linux..."
-        sudo cp -r /opt/zapret/init.d/runit/zapret/ /etc/sv/
-        sudo ln -s /etc/sv/zapret/ /etc/service/
-        sudo sv up zapret 
-        echo "Служба zapret настроена и запущена для AntiX Linux."
+      echo "Настройка службы zapret для AntiX Linux..."
+      sudo cp -r /opt/zapret/init.d/runit/zapret/ /etc/sv/
+      sudo ln -s /etc/sv/zapret/ /etc/service/
+      sudo sv up zapret
+      echo "Служба zapret настроена и запущена для AntiX Linux."
     fi
 
   fi
@@ -125,45 +125,84 @@ general_MGTS2() {
   default_install
 }
 
+general_FAKE_TLS_MOD() {
+  echo "Установка конфига general(FAKE TLS MOD)..."
+  if ! cp "$HOME/zapret-configs/configs/general_(FAKE_TLS_MOD)" /opt/zapret/config; then
+    echo "Ошибка: не удалось скопировать конфиг general(FAKE TLS MOD)."
+    exit 1
+  fi
+  default_install
+}
+
+general_FAKE_TLS_MOD_ALT() {
+  echo "Установка конфига general(FAKE TLS MOD ALT)..."
+  if ! cp "$HOME/zapret-configs/configs/general_(FAKE_TLS_MOD_ALT)" /opt/zapret/config; then
+    echo "Ошибка: не удалось скопировать конфиг general(FAKE TLS MOD ALT)."
+    exit 1
+  fi
+  default_install
+}
+
+general_FAKE_TLS_MOD_AUTO() {
+  echo "Установка конфига general(FAKE TLS MOD AUTO)..."
+  if ! cp "$HOME/zapret-configs/configs/general_(FAKE_TLS_MOD_AUTO)" /opt/zapret/config; then
+    echo "Ошибка: не удалось скопировать конфиг general(FAKE TLS MOD AUTO)."
+    exit 1
+  fi
+  default_install
+}
+
 # Меню для выбора конфига
 echo "Выберите конфиг для установки:"
-select config in "general" "general_ALT" "general_ALT2" "general_ALT3" "general_ALT4" "general_ALT5" "general_MGTS" "general_MGTS2"; do
+select config in "general" "general_ALT" "general_ALT2" "general_ALT3" "general_ALT4" "general_ALT5" "general_MGTS" "general_MGTS2" "general_FAKE_TLS_MOD" "general_FAKE_TLS_MOD_AUTO" "general_FAKE_TLS_MOD_ALT"; do
   case $config in
-    "general")
-      general
-      break
-      ;;
-    "general_ALT")
-      general_ALT
-      break
-      ;;
-    "general_ALT2")
-      general_ALT2
-      break
-      ;;
-    "general_ALT3")
-      general_ALT3
-      break
-      ;;
-    "general_ALT4")
-      general_ALT4
-      break
-      ;;
-    "general_ALT5")
-      general_ALT5
-      break
-      ;;
-    "general_MGTS")
-      general_MGTS
-      break
-      ;;
-    "general_MGTS2")
-      general_MGTS2
-      break
-      ;;
-    *)
-      echo "Неверный выбор. Пожалуйста, выберите снова."
-      ;;
+  "general")
+    general
+    break
+    ;;
+  "general_ALT")
+    general_ALT
+    break
+    ;;
+  "general_ALT2")
+    general_ALT2
+    break
+    ;;
+  "general_ALT3")
+    general_ALT3
+    break
+    ;;
+  "general_ALT4")
+    general_ALT4
+    break
+    ;;
+  "general_ALT5")
+    general_ALT5
+    break
+    ;;
+  "general_MGTS")
+    general_MGTS
+    break
+    ;;
+  "general_MGTS2")
+    general_MGTS2
+    break
+    ;;
+  "general_FAKE_TLS_MOD")
+    general_FAKE_TLS_MOD
+    break
+    ;;
+  "general_FAKE_TLS_MOD_AUTO")
+    general_FAKE_TLS_MOD_AUTO
+    break
+    ;;
+  "general_FAKE_TLS_MOD_ALT")
+    general_FAKE_TLS_MOD_ALT
+    break
+    ;;
+  *)
+    echo "Неверный выбор. Пожалуйста, выберите снова."
+    ;;
   esac
 done
 
